bab58c974ede9536306deb464d1d9e92ebc602fe
yeeuneey <yeeuneey@naver.com>
2025-11-05 00:24:18 +0900

    임시 커밋

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MyPageView from "../view/mypageView.vue";
 import ProfileSettingsView from "../view/profileSettingsView.vue";
+import UserLogin from "../view/UserLogin.vue";
 
 const routes = [
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    path: "/login",
+    name: "UserLogin",
+    component: UserLogin,
+  },
   {
     path: "/mypage",
     name: "MyPage",
