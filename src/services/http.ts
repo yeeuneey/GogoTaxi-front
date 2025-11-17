76c23984b6504161188eb7455960bdd127905743
yeeuneey <yeeuneey@naver.com>
2025-11-17 18:48:28 +0900

    오류 수정

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,10 +1,14 @@
-import axios from 'axios'
+﻿import axios from "axios"
 
-const apiBaseUrl = import.meta.env.VITE_API_URL?.replace(/\/$/, '')
+const apiBaseUrl = import.meta.env.VITE_API_URL?.replace(/\/$/, "")
 
 export const apiClient = axios.create({
   baseURL: apiBaseUrl || undefined,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 })
+
+// Alias to match existing imports (e.g., apiAuth.ts)
+export const http = apiClient
+export default apiClient
